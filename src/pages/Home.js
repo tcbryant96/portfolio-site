33c241f86ec6638765d2823adde41393129d61aa
tcbryant96 <108896490+tcbryant96@users.noreply.github.com>
2023-01-15 12:12:24 -0500

    bio-buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,34 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
 import "../App.css";
+import Button from 'react-bootstrap/Button';
+import * as AiIcons from "react-icons/ai"
+
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="row d-flex justify-content-center ">
-        <div className="col-3">
+      <div className="row">
+        <div className="col-6 d-flex justify-content-end">
           <Image
-            className="hero-image"
+            className="hero-image me-5"
             src={require("../images/me.jpg")}
             alt="Fridge"
           />
         </div>
-        <div className="col-5">
+        <div className="col mt-5">
           <h4>Hello World! I'm</h4><p><span style={{color:"blue"}}>Trevor</span> Bryant</p>    
           <p className="fs-5 ">A<span style={{color:"blue"}}> Front-End Web Devolper </span>& natural problem solver transitioning into the field of tech.</p>
+          <div className="row d-flex align-items-center">
+            <div className="col-4">
+          <Button size="lg" className="resume-button">Resumé</Button>
+          </div>
+          <div className="col-7 d-flex justify-content-evenly">
+            <Button size="md" className="bio-button"><AiIcons.AiFillLinkedin/></Button>
+            <Button className="bio-button"><AiIcons.AiOutlineGithub/></Button>
+            <Button className="bio-button d-flex justify-content-center align-items-center"><AiIcons.AiOutlineFacebook/></Button>
+          </div>
+          </div>
         </div>
       </div>
     </div>
