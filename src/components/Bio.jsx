21a926fe717b43d95dc0aae25ab3d5d525dea02e
tcbryant96 <108896490+tcbryant96@users.noreply.github.com>
2023-03-05 14:22:56 -0500

    minor updates

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -17,12 +17,10 @@ export default function Bio() {
         minBreakpoint="xxs"
       >
         <div
-          className="row d-flex justify-content-center align-items-center p-5 mb-5"
+          className="row column-container d-flex justify-content-center align-items-center pt-5 mb-5"
           id="my-bio"
         >
-          <div className="col d-flex flex-column justify-content-center align-items-center">
-            <div className="row d-flex justify-content-center align-items-center flex-grow-1">
-              <div className="col h-100 d-flex justify-content-center align-items-center">
+          <div className="col column-order-1 d-flex flex-column justify-content-center align-items-center">
                 <div style={{ position: "relative" }}>
                   <Image
                     className="hero-image"
@@ -48,31 +46,9 @@ export default function Bio() {
                     src={require("../images/—Pngtree—black mask design for free_6234134.png")}
                   ></Image>
                 </div>
-              </div>
-            </div>
-            <div className="row ">
-              <div className="col h-100 d-flex justify-content-center align-items-center pt-5">
-                <Button
-                  className=" mt-4 resume-button anime-button"
-                  onClick={() => {
-                    setToAnime(true);
-                  }}
-                >
-                  ANIME-te me
-                </Button>
-              </div>
-            </div>
           </div>
-          <div
-            xl={6}
-            lg={6}
-            md={12}
-            sm={12}
-            className="col-md-12 col-lg-6 mt-5 d-flex flex-column"
-          >
-            <div className="row flex-grow-1">
-              <div className="col  h-100">
-                <h2 className="mb-4 text-center ">Hello World! I'm</h2>
+          <div className="col-md-12 column-order-2 col-lg-6 mt-5 d-flex flex-column">
+                  <h2 className="mb-4 text-center ">Hello World! I'm</h2>
                 <h1
                   className={`mb-4 text-center ${
                     moveNameIn ? "animate-name" : "hidden"
@@ -88,41 +64,52 @@ export default function Bio() {
                   </span>
                   & natural problem solver transitioning into the field of tech.
                 </h2>
-              </div>
-            </div>
-            <div className="row h-100 d-flex justify-content-center align-items-center">
-              <div
-                lg={4}
-                md={4}
-                sm={12}
-                xs={12}
-                xxs={12}
-                className="col h-100 d-flex justify-content-center align-items-center"
-              >
-                <a href="https://docs.google.com/document/d/18RRgV2cI6T-Q4lCna-mMc4Hid6qq4WJ7C9C8NGP_Pl8/edit?usp=sharing">
-                  <Button size="lg" className="resume-button">
-                    Resumé
-                  </Button>
-                </a>
-              </div>
-              <div className="col d-flex  justify-content-center align-items-center">
-                <a href="https://www.linkedin.com/in/trevor-bryant-2589ba218/">
-                  <Button className="bio-button m-2">
-                    <AiIcons.AiFillLinkedin />
-                  </Button>
-                </a>
-                <a href="https://github.com/tcbryant96">
-                  <Button className="bio-button m-2">
-                    <AiIcons.AiOutlineGithub />
-                  </Button>
-                </a>
-                <a href="#contact-form">
-                  <Button className="bio-button m-2">
-                    <AiIcons.AiOutlineMail />
-                  </Button>
-                </a>
-              </div>
-            </div>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-md-12 col-lg-6 d-flex justify-content-center align-items-center">
+            {toAnime ?
+                 <Button
+                 size="lg"
+                 className=" resume-button anime-button mb-5"
+                 onClick={() => {
+                   setToAnime(false);
+                 }}
+               >
+                 Normalize Me
+               </Button>
+            :
+            <Button
+              size="lg"
+              className=" resume-button anime-button mb-5"
+              onClick={() => {
+                setToAnime(true);
+              }}
+            >
+              ANIME-te Me
+            </Button>}
+          </div>
+          <div className="col d-flex justify-content-evenly align-items-center mb-5">
+            <a href="https://docs.google.com/document/d/18RRgV2cI6T-Q4lCna-mMc4Hid6qq4WJ7C9C8NGP_Pl8/edit?usp=sharing">
+              <Button size="lg" className="resume-button">
+                Resumé
+              </Button>
+            </a>
+            <a href="https://www.linkedin.com/in/trevor-bryant-2589ba218/">
+              <Button className="bio-button m-2">
+                <AiIcons.AiFillLinkedin />
+              </Button>
+            </a>
+            <a href="https://github.com/tcbryant96">
+              <Button className="bio-button m-2">
+                <AiIcons.AiOutlineGithub />
+              </Button>
+            </a>
+            <a href="#contact-form">
+              <Button className="bio-button m-2">
+                <AiIcons.AiOutlineMail />
+              </Button>
+            </a>
           </div>
         </div>
       </ThemeProvider>
